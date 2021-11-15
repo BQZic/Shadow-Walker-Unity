c34596ee31905366e5be0e61aa873e349415b89b
191******91 <9393068+LittleAoAo@user.noreply.gitee.com>
2021-11-15 13:42:19 +0000

    update Assets/Scripts/Player.cs.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using LightObject;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -11,10 +8,13 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    public float speed=10f;
-    public float jumpingHeight=8f;
+    public float speed = 10f;
+    public float jumpingHeight = 8f;
     public float hp = 100;
+    public float minusHp = 0.01f;
+    public float lightBallLevel = 1f;
     private int jumpNum = 0;
+    private int Light = 0;
     private float xVelocity;
 
 
@@ -29,18 +29,34 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        xVelocity = Input.GetAxis("Horizontal");
+        if (xVelocity >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (xVelocity <= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (Light == 0)
+        {
+            if (xVelocity != 0)
+            {
+                hp -= minusHp;
+            }
+        }
         Movement();
         textLife.text = hp.ToString("00");
-        
+
         // 新加：
         CheckDeath();
     }
 
     void Movement()
     {
-        xVelocity = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
-        if (jumpNum ==1)
+        if (jumpNum == 1)
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
             {
@@ -48,7 +64,11 @@ public class Player : MonoBehaviour
                 jumpNum = 0;
             }
         }
-        
+        if (hp > 100)
+        {
+            hp = 100;
+        }
+
         // 新加：TODO - 由于跑步动画过长 需要中途停止？
         _animator.SetBool("isRun", rb.velocity != Vector2.zero);
     }
@@ -62,19 +82,34 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("Damage"))
         {
             hp -= 20;
-            if (hp<0)
+            if (hp < 0)
             {
                 Destroy(gameObject);
             }
         }
     }
 
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            Light = 1;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            Light = 0;
+        }
+    }
     // 新加：
     private void CheckDeath()
     {
         if (hp <= 0) Destroy(gameObject);
     }
-    
+
     public void TakeDamage(int damage)
     {
         hp -= damage;
@@ -83,5 +118,10 @@ public class Player : MonoBehaviour
     public float GetHP()
     {
         return hp;
+    }
+
+    public void AddHP()
+    {
+        hp += lightBallLevel;
     }
 }
