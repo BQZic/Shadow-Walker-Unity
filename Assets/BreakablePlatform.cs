06d60bd0200ea495cf19f7be4f4bb47aceaa02d0
BQZic <iceyzou99@gmail.com>
2021-11-24 23:01:12 +0800

    teleport; standing platform updates

---
+++ Assets/BreakablePlatform.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour
+{
+    private float _onBoardTime = 0f;
+    public float maxStandingTime = 3f;
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _onBoardTime += Time.deltaTime;
+            if (_onBoardTime >= maxStandingTime)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
